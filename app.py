919f38add925a9fec3007a093f7f9e7b36d3431f
Bishal Banerjee <56464152+ZenderGoD@users.noreply.github.com>
2022-11-30 23:43:13 +0530

    Update app.py

--- app.py
+++ app.py
@@ -3,23 +3,23 @@ import pandas as pd
 import streamlit as st
 
 def welcome():
-    return "Welcome All"
+    return "Welcome"
 
 def main():
-  st.title("Subtraction")
+  st.title("Addition")
   html_temp = """
   <div style="background-color:tomato;padding:10px">
-  <h2 style="color:white;text-align:center;">Subtraction of 2 numbers using Streamlit</h2>
+  <h2 style="color:white;text-align:center;">Addition of 2 numbers using Streamlit</h2>
   </div>
   """
   st.markdown(html_temp,unsafe_allow_html=True)
   num1 = st.number_input("Number 1")
   num2 = st.number_input("Number 2")
-  result = num1 - num2
-  st.success('The output is {}'.format(result))
+  result = num1 + num2
+  st.success('The result is {}'.format(result))
   if st.button("Made By"):
-      st.text("Parth Bansal")
-      st.text("21f3000805")
+      st.text("Bishal Banerjee")
+      st.text("21f2000514")
 
 if __name__=='__main__':
   main()
